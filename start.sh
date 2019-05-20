b5cab9d17a6faeaed75828fb5435b4c94938b1c3
Christian Zufferey <christian@zufferey.com>
2019-05-20 15:39:37 +0200

    Commencé à configurer la sonde blackbox-exporter pour le projet ressenti

---
+++ start.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#Petit script pour démarrer tout le binz
+#zf190520.1533
+
+#pour installer Docker et Docker compose sur une machine Ubuntu c'est ici:
+#https://github.com/zuzu59/docker_demo
+
+#source: https://hub.docker.com/r/prom/blackbox-exporter
+
+docker network create traefik
+docker-compose up
+#docker-compose up -d
+docker-compose logs -f
+
+
+echo -e "
+
+pour voir les logs en continu:
+docker-compose logs -f
+
+pour voir qu'est-ce qui tourne:
+docker-compose ps
+
+pour 'entrer' dans un 'service':
+docker-compose exec blackbox-exporter /bin/sh
+
+pour arrêter:
+docker-compose stop
+
+pour redémarrer après un 'stop':
+docker-compose start
+
+pour enlever les container mais pas les datas:
+docker-compose down
+
+pour enlever les container ET aussi les datas:
+docker-compose down -v --remove-orphans
+
+
+"
+
+
